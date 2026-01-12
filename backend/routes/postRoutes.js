85cf3763ef6d8db7e91810bb671e0dce66e3f942
LikhithaNasaka <nasakalikhitha@gmail.com>
2026-01-12 16:22:01 +0530

    Update postRoutes.js

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -5,11 +5,6 @@ const cloudinary = require('cloudinary').v2;
 const streamifier = require('streamifier');
 const Post = require('../models/Post');
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
 
 const upload = multer();
 
